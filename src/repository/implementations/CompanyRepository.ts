1b145ee5806e9fe0b487735a915f0fdb9ac1ef3d
Matheus Evangelista <matheus.evangelista@g.globo>
2022-06-09 16:04:22 -0300

    refactor: get the last documents from a collection

--- src/repository/implementations/CompanyRepository.ts
+++ src/repository/implementations/CompanyRepository.ts
@@ -5,12 +5,12 @@ import { ICompanyRepository } from "../ICompanyRepository";
 
 export class CompanyRepository implements ICompanyRepository {
     async fetchVacanciesByCompany(company: string): Promise<Document[]> {
-        const response = await Vacancy.find({ company })
+        const response = await Vacancy.find({ company }).sort({'_id':-1})
         return response
     }
 
     async fetchVacancies(): Promise<Document[]>  {
-        const response = await Vacancy.find()
+        const response = await Vacancy.find().sort({'_id':-1})
         return response
     }
 
